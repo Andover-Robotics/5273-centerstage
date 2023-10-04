b66672b68daae211a1efda454f80a6c69a8db69c
dshi2026 <dshi2008mayo@gmail.com>
2023-10-04 16:55:13 -0400

    Interfaces and classes for fly wheel and hanging motor.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RealFlyWheel.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+public class RealFlyWheel implements FlyWheel {
+    public void on()
+    {
+        // ToDo: Turn the fly wheel on
+    }
+
+    public void off()
+    {
+        // ToDo: Turn the fly wheel off
+    }
+
+    public onOffState getState()
+    {
+        // ToDo: return whether the fly wheel is on or off
+        return null;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RealHangingMotor.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+public class RealHangingMotor implements HangingMotor {
+    @Override
+    public void on() {
+        // ToDo: turn the motor on
+    }
+
+    @Override
+    public void off() {
+        // ToDo: turn the motor off
+    }
+
+    @Override
+    public onOffState getState() {
+        // ToDo: get the state of the motor and return
+        return null;
+    }
+}
