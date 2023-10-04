788d2d73066727bd26cce6bbf08fd00a9876c306
dshi2026 <dshi2008mayo@gmail.com>
2023-10-04 17:23:34 -0400

    made bot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/Bot.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.bot;
+
+import org.firstinspires.ftc.teamcode.hardware.FlyWheel;
+import org.firstinspires.ftc.teamcode.hardware.HangingMotor;
+
+public interface Bot {
+}
