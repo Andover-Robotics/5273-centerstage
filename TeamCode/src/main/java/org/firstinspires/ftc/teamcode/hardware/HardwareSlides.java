11b991b8f3f962c934b9dde9783de3a5092210f7
ngerow2025 <ngerow12025@gmail.com>
2023-10-04 16:39:58 -0400

    add hardware interfaces and scafolding for slides and mechanum drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareSlides.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+public interface HardwareSlides {
+    void setSlidesPower(double power);
+    int getSlidesPosition();
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RealHardwareMechanumDrive.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+public class RealHardwareMechanumDrive implements HardwareMechanumDrive {
+
+    @Override
+    public void setDrivePower(double leftFront, double rightFront, double leftBack, double rightBack) {
+        //TODO: Implement for real hardware motors
+    }
+}
