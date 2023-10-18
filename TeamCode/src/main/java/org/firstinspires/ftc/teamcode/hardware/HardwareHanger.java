d95495751675a5a63ee3d9954f6744ff67bf774a
e <ethanlazar42@gmail.com>
2023-10-18 15:16:52 -0400

    added hanger methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareHanger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareHanger.java
@@ -1,13 +1,13 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 public interface HardwareHanger {
-    public enum onOffState {
-        ON,
-        OFF
-    }
-
-    public void on();
-    public void off();
-    public onOffState getState();
+    int getTargetPosition();
+    boolean isBusy();
+    void setTargetPosition(int position);
+    DcMotorSimple.Direction getDirection();
+    double getPower();
+    void setDirection(DcMotorSimple.Direction direction);
+    void setPower(double power);
 }
-
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RealHardwareHanger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RealHardwareHanger.java
@@ -1,19 +1,41 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 public class RealHardwareHanger implements HardwareHanger {
-    @Override
-    public void on() {
-        // ToDo: turn the motor on
+    private final DcMotor motor;
+    public RealHardwareHanger(){
+        motor = hardwareMap.get(DcMotor.class, "hangerMotor")
     }
-
     @Override
-    public void off() {
-        // ToDo: turn the motor off
+    public int getTargetPosition(){
+        return motor.getTargetPosition();
     }
-
     @Override
-    public onOffState getState() {
-        // ToDo: get the state of the motor and return
-        return null;
+    public boolean isBusy(){
+        return motor.isBusy();
+    }
+    @Override
+    public void setTargetPosition(int position){
+        motor.setTargetPosition(position);
+    }
+    @Override
+    public DcMotorSimple.Direction getDirection(){
+        return motor.getDirection();
+    }
+    @Override
+    public double getPower(){
+        return motor.getPower();
+    }
+    @Override
+    public void setDirection(DcMotorSimple.Direction direction){
+        motor.setDirection(direction);
+    }
+    @Override
+    public void setPower(double power){
+        motor.setPower(power);
     }
 }
