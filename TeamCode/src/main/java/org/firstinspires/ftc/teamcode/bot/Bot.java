65cb15208395db2bbe3f973ca3660aa8f1e72970
ngerow2025 <ngerow12025@gmail.com>
2023-11-04 19:56:40 -0400

    finish Bot class for movement only

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/Bot.java
@@ -1,7 +1,15 @@
 package org.firstinspires.ftc.teamcode.bot;
 
+import org.firstinspires.ftc.teamcode.subsystems.*;
+
 public class Bot {
-    public HardwareBot hardwareBot;
+    public Claw claw;
+    public Camera camera;
+    public Hanger hanger;
+    public Launch launch;
+    public Movement movement;
+    public Slides slides;
+
     public Bot(HardwareBot hardwareBot) {
         assert hardwareBot.claw != null;
         assert hardwareBot.flyWheel != null;
@@ -9,7 +17,12 @@ public class Bot {
         assert hardwareBot.launch != null;
         assert hardwareBot.mecanumDrive != null;
         assert hardwareBot.slides != null;
-        this.hardwareBot = hardwareBot;
+//        claw = new Claw(hardwareBot.claw);
+//        camera = new Camera();
+//        hanger = new Hanger(hardwareBot.hanger);
+//        launch = new Launch(hardwareBot.launch);
+        movement = new Movement(0, 0, 0, hardwareBot.mecanumDrive);
+//        slides = new Slides(hardwareBot.slides);
     }
 
     public void place_pixel_ground() {}
