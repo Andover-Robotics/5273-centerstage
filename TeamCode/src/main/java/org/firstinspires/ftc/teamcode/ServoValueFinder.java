5375ef30bdedb513be9b998dd2f092c96625ef0f
Nathan Gerow <ngerow12025@gmail.com>
2023-12-29 16:58:09 -0500

    improved and fixed ServoValueFinder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoValueFinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoValueFinder.java
@@ -5,38 +5,43 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Servo Value Finder", group = "Teleop")
-public class LauncherValueFinder extends LinearOpMode {
+public class ServoValueFinder extends LinearOpMode {
     private static final String[] SERVO_LIST = {
             "clawServo",
             "flipperServo",
             "launchServo"
     };
     private static int num = 0;
+    private boolean dpad_right_prev = false;
+    private boolean dpad_left_prev = false;
     @Override
     public void runOpMode() throws InterruptedException {
-        waitForStart();
-        while(!gamepad2.a){
-            if(gamepad2.dpad_right){
+        while(!gamepad2.a && !isStopRequested()){
+            if(gamepad2.dpad_right && !dpad_right_prev){
                 num = (num + 1) % SERVO_LIST.length;
-                //TODO: fix this so it does not block
-                //blocking will make nothing else work
-                //and also if stop is pressed it will not stop
-                while(gamepad2.dpad_right){}
-            }else if(gamepad2.dpad_left){
-                num = (num + SERVO_LIST.length - 1) % SERVO_LSIT.length;
-                while(gamepad2.dpad_left){}
+                dpad_right_prev = true;
+            }else if(gamepad2.dpad_left && !dpad_left_prev){
+                num = (num + SERVO_LIST.length - 1) % SERVO_LIST.length;
+                dpad_left_prev = true;
+            }
+            if(!gamepad2.dpad_right){
+                dpad_right_prev = false;
+            }
+            if(!gamepad2.dpad_left){
+                dpad_left_prev = false;
             }
             telemetry.addData("selected servo: ", SERVO_LIST[num]);
             telemetry.update();
         }
         Servo servo = hardwareMap.get(Servo.class, SERVO_LIST[num]);
+        waitForStart();
         while(opModeIsActive()){
             if(gamepad2.dpad_up){
-                servo.setPosition(claw.getPosition()+0.0001);
+                servo.setPosition(servo.getPosition()+0.0001);
             }else if(gamepad2.dpad_down){
-                servo.setPosition(claw.getPosition()-0.0001);
+                servo.setPosition(servo.getPosition()-0.0001);
             }
-            telemetry.addData("servo pos: ",claw.getPosition());
+            telemetry.addData("servo pos: ",servo.getPosition());
             telemetry.update();
         }
     }
