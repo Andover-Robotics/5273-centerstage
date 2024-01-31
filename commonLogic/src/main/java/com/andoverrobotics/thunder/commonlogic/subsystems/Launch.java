1dff20280a06f6714a3f267deefb8ac78ab92e2f
Nathan Gerow <ngerow12025@gmail.com>
2024-01-30 19:55:32 -0500

    tuned launch

--- commonLogic/src/main/java/com/andoverrobotics/thunder/commonlogic/subsystems/Launch.java
+++ commonLogic/src/main/java/com/andoverrobotics/thunder/commonlogic/subsystems/Launch.java
@@ -3,21 +3,8 @@ package com.andoverrobotics.thunder.commonlogic.subsystems;
 import com.andoverrobotics.thunder.commonlogic.hardwareInterfaces.HardwareLaunch;
 
 public class Launch {
-    private class LaunchThread extends Thread{
-        public void run(){
-            launching=true;
-            hardwareLaunch.setPosition(LAUNCH_ON);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            hardwareLaunch.setPosition(LAUNCH_OFF);
-            launching=false;
-        }
-    }
-    private static final double LAUNCH_ON = 0.5;
-    private static final double LAUNCH_OFF = 0.0;
+    private static final double LAUNCH_ON = 0.05;
+    private static final double LAUNCH_OFF = 0.36833333333;
     private boolean launching;
     private final HardwareLaunch hardwareLaunch;
 
@@ -27,7 +14,20 @@ public class Launch {
     }
     public void executeIntent(boolean launch){
         if(launch && !launching){
-            new LaunchThread().start();
+            launching = true;
+            new Thread(){
+                public void run(){
+                    launching=true;
+                    hardwareLaunch.setPosition(LAUNCH_ON);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    hardwareLaunch.setPosition(LAUNCH_OFF);
+                    launching=false;
+                }
+            }.start();
         }
     }
 }
